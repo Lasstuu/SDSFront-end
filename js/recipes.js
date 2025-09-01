0bf75cee761feeeeec314835fc530fc342c28901
Lasstuu <lassituo@gmail.com>
2025-09-01 15:23:50 +0300

    Mobile navigation menu for recipes tab also

--- js/recipes.js
+++ js/recipes.js
@@ -22,3 +22,11 @@
     recipesGrid.appendChild(card);
     form.reset();
   });
+  // Mobile Menu
+document.addEventListener("DOMContentLoaded", () => {
+  const mobileMenuButton = document.querySelector(".mobile-menu-button");
+  const mobileMenu = document.querySelector(".mobile-menu");
+  mobileMenuButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+  });
+});
